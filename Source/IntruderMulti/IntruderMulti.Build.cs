1ca396b065df32b64d1988bbd5e4b5a01f74edc5
Bertolen <metroidbruno@hotmail.com>
2017-12-27 19:26:45 +0100

    Removing Steam?
    Works locally on one computer, but will it work on two of them?

--- Source/IntruderMulti/IntruderMulti.Build.cs
+++ Source/IntruderMulti/IntruderMulti.Build.cs
@@ -11,10 +11,5 @@ public class IntruderMulti : ModuleRules
 		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay", "UMG" });
 
         PrivateDependencyModuleNames.AddRange(new string[] { "OnlineSubsystem", "OnlineSubsystemUtils", "OnlineSubsystemNull" });
-
-        if ((Target.Platform == UnrealTargetPlatform.Win32) || (Target.Platform == UnrealTargetPlatform.Win64))
-        {
-            DynamicallyLoadedModuleNames.Add("OnlineSubsystemSteam");
-        }
 	}
 }
