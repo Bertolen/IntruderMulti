429a81301cf64d20fead0ec203eebd7bb6c836b2
Bertolen <metroidbruno@hotmail.com>
2018-01-15 23:20:13 +0100

    Nerfed the Thief's jump

--- Source/IntruderMulti/Characters/TP_Characters/Thief.cpp
+++ Source/IntruderMulti/Characters/TP_Characters/Thief.cpp
@@ -17,12 +17,6 @@ AThief::AThief(const FObjectInitializer& ObjectInitializer)
 	bUseControllerRotationYaw = true;
 	bUseControllerRotationRoll = false;
 
-	// Configure character movement
-	GetCharacterMovement()->bOrientRotationToMovement = true; // Character moves in the direction of input...	
-	GetCharacterMovement()->RotationRate = FRotator(0.0f, 540.0f, 0.0f); // ...at this rotation rate
-	GetCharacterMovement()->JumpZVelocity = 600.f;
-	GetCharacterMovement()->AirControl = 0.2f;
-
 	// Create a camera boom (pulls in towards the player if there is a collision)
 	CameraBoom = CreateDefaultSubobject<USpringArmComponent>(TEXT("CameraBoom"));
 	CameraBoom->SetupAttachment(RootComponent);
